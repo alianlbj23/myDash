2654544b8ac0bdb453f04ce4b1a28c886951f76e
alianlbj23 <alianlbj23@gmail.com>
2022-03-12 20:15:57 +0800

    新增app.py

--- app.wsgi
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.insert(0, r'/var/www/Dash_test_myDash')
-from dapp import app as application
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0,"/var/www/Dash_test/myDash/")
+from app import server as application
